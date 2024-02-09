410334f7142d9993c92cc93a59241b760d88feda
Amit5153 <125106028+Amit5153@users.noreply.github.com>
2024-02-09 13:21:57 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,16 @@
+// "use strict";   treat all js a new version
+// alert(3+3)
+
+// some datatypes are 
+
+// number 2power to 53
+// strring  " hey are you there"
+// bool     yes or no
+// null
+// undefined 
+// object 
+// typeof
+
+// console.log(typeof undefined);       undefined 
+
+// console.log(typeof null);   object
