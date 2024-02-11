831ff09b86f79bf1809c42fca6c4fec859d7a6f3
Amit5153 <125106028+Amit5153@users.noreply.github.com>
2024-02-11 11:57:58 +0000

    conatiner config

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,35 @@
+// // primtitive data types.
+// //  string , number , boolean , Null , undefined , symbol, BigInt
+
+
+// // const score = false 
+
+// // statically ya dynamicaly hai js  give ans bolo bolo .....
+
+// // const isLoggedIn = false;    boolean 
+// // let useremail;   undefined
+// const Id = Symbol('123 ')
+// const anotherId = Symbol('123')
+// console.log(Id == anotherId);
+
+
+// // const bigNumber = 34343494324242356n
+
+
+// // reference type (non primitive)
+
+// //  Array , objects, functions
+
+// const heros =["shark" ,"shaktiman", "nagaarjun", "doga"]
+
+//     let obj  = {
+//    name: "amit",
+//    age : 22,
+
+// }
+
+// const myFunction = function(){
+
+//     console.log(" hey everyone")
+// }
+
